9510f74d877c8f1812e104d1cab7f8cbff0aec5c
PebblesDV <pebblesdevries@gmail.com>
2024-10-06 15:37:28 +0200

    favicon

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default async function Contact() {
+  return (
+    <div className="">
+      <h1>Contact</h1>
+    </div>
+  );
+}
---
+++ app/over-mij/page.tsx
@@ -0,0 +1,7 @@
+export default async function About() {
+  return (
+    <div className="">
+      <h1>Over mij</h1>
+    </div>
+  );
+}
