b518578f5fb0e089321ad27fa83a5ff1de771938
PebblesDV <pebblesdevries@gmail.com>
2024-10-06 17:01:09 +0200

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 export default async function Home() {
   return (
-    <div className="">
-      <h1>Home</h1>
-    </div>
+    <main className="">
+      <div className="flex flex-col m-auto">
+        <p className="font-bold text-2xl w-2/5 text-center">
+          Student
+          <span className="text-primary">&#123;Web-Development&#125;</span> aan
+          het Grafisch Lyceum Utrecht
+        </p>
+
+        <p className="w-3/5 text-center">
+          Hoi! Ik ben Pebbles de Vries, een MBO Web-Development student. Welkom
+          bij mijn portfolio. Hier laat ik projecten zien waar ik trots op ben,
+          en die goed aantonen wat ik tot nu toe kan en waar ik mee bezig ben!
+        </p>
+      </div>
+
+      <div></div>
+    </main>
   );
 }
