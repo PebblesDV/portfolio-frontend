8bf620af20c215d653f575a997e57cab93c8b3d5
PebblesDV <pebblesdevries@gmail.com>
2024-10-07 12:38:06 +0200

    card hover fix

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -3,35 +3,33 @@ import Link from "next/link";
 
 export default function Card() {
   return (
-    <Link href={"projecten"} className="drop-shadow-custom-gray">
-      <div className="group">
-        <div className="w-full rounded-t-2xl overflow-hidden">
-          <Image
-            src="/code.webp"
-            width={0}
-            height={0}
-            alt="card-img"
-            sizes="100vw"
-            style={{
-              width: "100%",
-              height: "250px",
-              objectFit: "cover",
-              borderRadius: "20px 20px 0px 0px",
-            }}
-            className="hover:scale-105 transition duration-200"
-          />
-        </div>
+    <Link href={"projecten"} className="drop-shadow-custom-gray group">
+      <div className="w-full rounded-t-2xl overflow-hidden">
+        <Image
+          src="/code.webp"
+          width={0}
+          height={0}
+          alt="card-img"
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "250px",
+            objectFit: "cover",
+            borderRadius: "20px 20px 0px 0px",
+          }}
+          className="group-hover:scale-105 transition duration-200"
+        />
+      </div>
 
-        <div className="bg-cards rounded-b-2xl flex justify-between px-5 py-2">
-          <p className="text-lg font-medium">Projecten</p>
-          <Image
-            src="/arrow-right.svg"
-            width={20}
-            height={20}
-            alt="arrow"
-            className="group-hover:-rotate-45 transition-transform duration-200"
-          />
-        </div>
+      <div className="bg-cards rounded-b-2xl flex justify-between px-5 py-2">
+        <p className="text-lg font-medium">Projecten</p>
+        <Image
+          src="/arrow-right.svg"
+          width={20}
+          height={20}
+          alt="arrow"
+          className="group-hover:-rotate-45 transition-transform duration-200"
+        />
       </div>
     </Link>
   );
