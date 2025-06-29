a76b793d78deb3489136e9d22f28c8a0140b905b
PebblesDV <pebblesdevries@gmail.com>
2025-06-29 14:44:44 +0200

    strapi info projects page

--- app/projecten/page.tsx
+++ app/projecten/page.tsx
@@ -1,11 +1,6 @@
-import {
-  getAllProjects as oldGetAllProjects,
-  getFeaturedMediaById,
-} from "@/lib/wordpress";
 import { Metadata } from "next";
 import Card from "../components/card";
-import FallBackImage from "@/public/code.webp";
-import { getAllProjects } from "@/lib/strapi";
+import { getAllProjects, getFullImageUrl } from "@/lib/strapi";
 
 export const metadata: Metadata = {
   title: "Pebbles de Vries - Projecten",
@@ -23,28 +18,22 @@ export const metadata: Metadata = {
 };
 
 export default async function Projects() {
-  const oldProjects = await oldGetAllProjects();
   const projects = await getAllProjects();
 
-  console.log(JSON.stringify(projects, null, 2));
+  console.log(getFullImageUrl(projects.data[0].Cover.url));
+  console.log(projects.data[0].documentId);
+  console.log(projects.data[0].Title);
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-7 py-12">
-      {oldProjects.length > 0 &&
-        oldProjects.map(async (project) => {
-          const media =
-            project && project.acf && project.acf.card_image
-              ? typeof project.acf.card_image === "string"
-                ? FallBackImage
-                : (await getFeaturedMediaById(project.acf.card_image))
-                    .source_url
-              : FallBackImage;
+      {projects.data.length > 0 &&
+        projects.data.map(async (project) => {
           return (
             <Card
-              imageSrc={media}
-              key={project.id}
-              title={project.title.rendered}
-              href={`/projecten/${project.slug}`}
+              imageSrc={getFullImageUrl(project.Cover.url)}
+              key={project.documentId}
+              title={project.Title}
+              href={`/projecten/${project.documentId}`}
             />
           );
         })}
--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,9 +1,13 @@
 import { strapi } from "@strapi/client";
 
 const client = strapi({
-  baseURL: process.env.STRAPI_URL!,
+  baseURL: process.env.STRAPI_URL! + "/api",
   auth: process.env.STRAPI_TOKEN,
 });
+
+export const getFullImageUrl = (path: string) => {
+  return process.env.STRAPI_URL + path;
+};
 
 const projects = client.collection("projects");
 
